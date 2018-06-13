e9666d52b3a88095cb3374f099484b6fa2b31451
Katerina <ekurochka@gmail.com>
2018-06-13 16:25:27 +0300

    Задачи 1 и 2 done. Задача 3 в процессе

---
+++ store/js/ShopItem.js
@@ -0,0 +1,19 @@
+const ShopItem = function({item}) {
+  const {brand, title, description, descriptionFull, price, currency} = item;
+  return (
+    <div className="main-content">
+      <h2> {brand} </h2>
+  		<h1> {title} </h1>
+  		<h3> {description} </h3>
+  		<div class="description">
+   			{descriptionFull}
+  		</div>
+  		<div className="highlight-window  mobile"><div className="highlight-overlay"></div></div>
+  		<div className="divider"></div>
+  		<div className="purchase-info">
+    		<div className="price"> {currency}{price.toFixed(2)} </div>
+    		<button>Добавить в корзину</button>
+  		</div>
+		</div>
+  );
+}
